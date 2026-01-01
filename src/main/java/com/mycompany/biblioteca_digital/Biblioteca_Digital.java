6f7fae4686d554e8afeed2b48d4dd447ba1a96ab
altas123linux <altas123gc@gmail.com>
2026-01-01 16:55:36 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/biblioteca_digital/Biblioteca_Digital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.biblioteca_digital;
+
+/**
+ *
+ * @author ALEJANDRO
+ */
+public class Biblioteca_Digital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
